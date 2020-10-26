3b1d8924eed36bc65a28d42e5b0de624be3f9063
mda <mdlhy@qq.com>
2020-10-26 09:37:03 +0800

    随便改啊

--- ap1/models.py
+++ ap1/models.py
@@ -39,3 +39,21 @@ class midInhabitant(models.Model):
     ownerType = models.CharField(max_length=255, null=True, blank=True, choices=OWNER_CHOICES, verbose_name='与业主关系类型')
     status = models.CharField(max_length=255, null=True, blank=True, choices=STATUS_CHOICES, verbose_name='状态')
     sync_type = models.CharField(max_length=255, null=True, blank=True, verbose_name='同步类型')
+
+
+class midAdmin(models.Model):
+    SEX_CHOICES = (
+        (1, '男'),
+        (0, '女'),
+        (2, '未知')
+    )
+    id = models.AutoField(primary_key=True, verbose_name='主键')
+    username = models.CharField(max_length=255, blank=True, null=True, verbose_name='用户名')
+    pwd = models.CharField(max_length=255, null=True, blank=True, verbose_name='密码')
+    email = models.CharField(max_length=255, null=True, blank=True, verbose_name='电子邮箱')
+    mobile = models.CharField(max_length=255, null=True, blank=True, verbose_name='手机号')
+    family_name = models.CharField(max_length=255, null=True, blank=True,  verbose_name='姓')
+    last_name = models.CharField(max_length=255, null=True, blank=True,  verbose_name='名')
+    sex = models.CharField(max_length=255, null=True, blank=True, choices=SEX_CHOICES, verbose_name='性别')
+    id_card = models.CharField(max_length=255, null=True, blank=True, verbose_name='身份证')
+    sync_type = models.CharField(max_length=255, null=True, blank=True, verbose_name='同步类型')
